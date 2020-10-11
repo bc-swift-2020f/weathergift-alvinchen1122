80150be23bde1323695651bd9ac8a53d0c6a4562
Alvin Chen <chenbog@bc.edu>
2020-10-11 18:17:47 -0400

    with .gitignore

---
+++ WeatherGift/APIkeys.swift
@@ -0,0 +1,12 @@
+//
+//  APIkeys.swift
+//  WeatherGift
+//
+//  Created by Alvin Chen on 10/11/20.
+//
+
+import Foundation
+
+struct APIkeys {
+    static let googlePlacesKey = "AIzaSyC7KTIsn_n4CAFgK3kzNfHSVf8t03PXbPE"
+}
---
+++ WeatherGift/WeatherLocation.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherLocation.swift
+//  WeatherGift
+//
+//  Created by Alvin Chen on 10/10/20.
+//
+
+import Foundation
+
+struct WeatherLocation {
+    var name: String
+    var latitude: Double
+    var longitude: Double
+}
